0d6adef0829d686886a1dc07bf9f0c3083ef80e9
sapnil006 <55348917+sapnil006@users.noreply.github.com>
2021-08-21 20:51:25 +0530

    fix the issue when resources are insufficient still asking for payment

--- coffeemachine.py
+++ coffeemachine.py
@@ -1,19 +1,26 @@
 from menu import Menu
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
-menu = Menu()
-coffee_maker = CoffeeMaker()
+
 money_machine = MoneyMachine()
+coffee_maker = CoffeeMaker()
+menu = Menu()
 
+is_on = True
 
-menu_available = menu.get_items()
-print(f"What would you like :{menu_available}: ")
-choice = input("Enter your drink: ")
-your_drink = menu.find_drink(choice)
+while is_on:
+    options = menu.get_items()
+    choice = input(f"What would you like? ({options}): ")
+    if choice == "off":
+        is_on = False
+    elif choice == "report":
+        coffee_maker.report()
+        money_machine.report()
+    else:
+        drink = menu.find_drink(choice)
+        is_enough_ingredients = coffee_maker.is_resource_sufficient(drink)
 
+        if is_enough_ingredients and  money_machine.make_payment(drink.cost):
+            coffee_maker.make_coffee(drink)
 
-coffee_maker.report()
-if coffee_maker.is_resource_sufficient(your_drink):
-    if money_machine.make_payment(your_drink.cost):
-        coffee_maker.make_coffee(your_drink)
 
